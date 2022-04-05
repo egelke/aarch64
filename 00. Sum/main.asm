80fcff9e4f2e54005df1ef99db5672ae4cf23d63
bryan <info@egelke.net>
2022-04-05 14:56:18 +0200

    Cleanup tabs

--- 00. Sum/main.asm
+++ 00. Sum/main.asm
@@ -1,20 +1,19 @@
-	.arch armv8-a
+    .arch armv8-a
 
-	.text
-	.p2align 2
+    .text
+    .p2align 2
 
-	//entry point (see linker)
-	.global sum
-	//.type sum, %function
+    //entry point (see linker)
+    .global sum
 sum:
-	//sum 2 constants
-	mov x10, #1
-	mov x11, #1
-	add x9, x10, x11
+    //sum 2 constants
+    mov x10, #1
+    mov x11, #1
+    add x9, x10, x11
 
-	//return success (0)
-	mov w0, wzr
+    //return success (0)
+    mov w0, wzr
 
-	ret
+    ret
 
-	.end
+    .end
